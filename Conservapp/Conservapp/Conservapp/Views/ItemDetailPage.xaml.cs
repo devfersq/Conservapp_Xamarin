2f4bc93b1fc1d7e29d38e734c004d6518373ebd1
Fernando Sanchez Q <dev.fersq@outlook.com>
2022-01-24 23:43:59 -0600

    The mvvm architecture was created in the project.

---
+++ Conservapp/Conservapp/Conservapp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Conservapp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Conservapp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Conservapp/Conservapp/Conservapp/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Conservapp.Models;
+using Conservapp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conservapp.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
